bc316fb19a1f7cd468cca3e23cb8689a85a80374
at-rooshee <ru_sha_nia@yahoo.ca>
2017-06-12 20:38:26 +0300

    Adding json data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+let users = [];
+
+fetch(endpoint)
+ .then(blob => blob.json())
+ .then(data => users.push(...data))
+ 
+ function findMatches(wordToMatch, users) {
+   return users.filter(place => {
+     const regex = new RegExp(wordToMatch, 'gi');
+     return place.city.match(regex) || place.state.match(regex)
+   });
+ }
+
 class App extends Component {
   render() {
     return (
@@ -11,7 +26,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <input> {findMatches()}</input>
         </p>
       </div>
     );
